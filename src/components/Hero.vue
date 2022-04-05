16e3f476d1ebbda9ec966adf75ab0bc9081e435b
kevineyon1 <kevingideon123@gmail.com>
2022-04-05 07:09:25 +0700

    first commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row mt-4">
+    <div class="col">
+      <div class="d-flex h-100">
+        <div class="justify-content-center align-self-center">
+          <h2><b>Delicious Food And Drink</b> <br />In Your Gadget</h2>
+          <p>Welcome To CrimsonCafe !</p>
+          <button class="btn btn-lg btn-success">
+            <b-icon-arrow-right></b-icon-arrow-right>Order Now !
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <img src="../assets/images/hero.png" alt="" width="70%" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style>
+</style>
